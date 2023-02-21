76bc095a24e3c574d5f481172170f40261508df7
Alessio Rosiello <alex.robassomero@gmail.com>
2023-02-21 22:30:22 +0100

    Add lib_SoC_board

--- RTE/_uvision_exam/RTE_Components.h
+++ RTE/_uvision_exam/RTE_Components.h
@@ -16,8 +16,6 @@
  */
 #define CMSIS_device_header "LPC17xx.h"
 
-/* Keil::Device:Startup:1.0.0 */
-#define RTE_DEVICE_STARTUP_LPC17XX      /* Device Startup for NXP17XX */
 
 
 #endif /* RTE_COMPONENTS_H */
